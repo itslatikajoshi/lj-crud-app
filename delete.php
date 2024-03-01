1504170ac55c4f3e4d4b285f19bc64b77b89d023
Latika Joshi <itslatikajoshi@gmail.com>
2024-03-01 14:20:50 +0530

    delete functionality

--- delete.php
+++ delete.php
@@ -3,3 +3,16 @@ include "./db-conn.php";
 
 $id = $_GET['id'];
 $sql = "DELETE FROM contacts WHERE id = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("i", $id);
+$stmt->execute();
+
+if ($stmt->affected_rows > 0) {
+    header("Location: /crud/");
+    exit(); // Ensure no further execution of the script
+} else {
+    echo "Error deleting contact: " . $conn->error;
+}
+
+$stmt->close();
+$conn->close();
