ae486ded042c6ef35b152a1db374d9364600ac3d
Gaurav Joshi <30743792+gauravrjoshi@users.noreply.github.com>
2024-03-04 15:53:10 +0530

    Improvements

--- functions.php
+++ functions.php
@@ -8,22 +8,23 @@ function read($conn, $id)
     $sql = "SELECT * FROM `contacts` ORDER BY id DESC";
     $result = $conn->query($sql);
     $count = 1;
+    $html = "";
     if ($result->num_rows > 0) {
         // output data of each row
 
         //   print_r($row);die();
         while ($row = $result->fetch_assoc()) {
-
-            echo "<tr>
-  <td>" . $count . "</td>
-  <td>" . $row["name"] . "</td>
-  <td>" . $row["email"] . "</td>
-  <td>" . $row["phone"] . "</td>
-  <td>" .
-                "<a class='btn btn-secondary' href='./update.php?id=" . $row["id"] . "'>Update </a> " .
-                "<a class='btn btn-danger cl-delete' href='./delete.php?id=" . $row["id"] . "'>Delete </a>
-  </td>
-  </tr>";
+            $html .= "<tr>";
+            $html .= "<td>" . $count . "</td>";
+            $html .= "<td>" . $row["name"] . "</td>";
+            $html .= "<td>" . $row["email"] . "</td>";
+            $html .= "<td>" . $row["phone"] . "</td>";
+            $html .= "<td>";
+            $html .= "<a class='btn btn-secondary' href='./update.php?id=" . $row["id"] . "'>Update </a> ";
+            $html .= "<a class='btn btn-danger cl-delete' href='./delete.php?id=" . $row["id"] . "'>Delete </a>";
+            $html .= "</td>";
+            $html .= "</tr>";
+            echo $html;
             $count++;
         }
     } else {
