872681cd091e4a848a11b4bdc2e33e7e89a24800
Gaurav Joshi <30743792+gauravrjoshi@users.noreply.github.com>
2024-03-04 15:50:55 +0530

    Changes By Gauav J

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 include "./db-conn.php";
+include "./functions.php";
 ?>
 
 <!DOCTYPE html>
@@ -61,40 +62,11 @@ include "./db-conn.php";
           </tr>
 
           <?php
-          $sql = "SELECT * FROM `contacts` ORDER BY id DESC";
-          $result = $conn->query($sql);
-          $count = 1;
-          if ($result->num_rows > 0) {
-            // output data of each row
-
-            //   print_r($row);die();
-            while ($row = $result->fetch_assoc()) {
-
-              echo "<tr>
-        <td>" . $count . "</td>
-        <td>" . $row["name"] . "</td>
-        <td>" . $row["email"] . "</td>
-        <td>" . $row["phone"] . "</td>
-        <td>" .
-                "<a class='btn btn-secondary' href='./update.php?id=" . $row["id"] . "'>Update </a> " .
-                "<a class='btn btn-danger cl-delete' href='./delete.php?id=" . $row["id"] . "'>Delete </a>
-        </td>
-        </tr>";
-              $count++;
-            }
-          } else {
-            echo "<tr><td colspan='5' class='text-center'>No result found</td></tr>";
-            // echo "0 results";
-          }
+          read($conn);
           ?>
         </table>
       </div>
     </div>
-
-
-
-
-
   </div>
 
   <script src="./script.js"></script>
