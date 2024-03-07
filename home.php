431522aa50c3fd177c21c4b229f96ceb30ee63aa
Latika Joshi <itslatikajoshi@gmail.com>
2024-03-07 14:58:27 +0530

    Login page added

---
+++ home.php
@@ -0,0 +1,101 @@
+<?php
+include "./db-conn.php";
+include "./functions.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>CRUD app</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col-sm-4">
+        <!--
+            NOTES: By default form method is GET
+            Array
+            (
+            [fullname] => Latika
+            [email] => itslatika@gmail.com
+            [phone] => 876543345
+            )
+            -->
+        <div class="card">
+          <div class="card-header">
+            Please fill below details
+          </div>
+          <div class="card-body">
+            <?php
+            if (isset($_GET["error"])) : ?>
+              <div class="lj-errors">
+                <div class="alert alert-danger" role="alert">
+                 <?php echo $_GET["error"];?>
+                </div>
+              </div>
+            <?php
+            endif; ?>
+            <form action="create.php" class="lj-submit-form" method="POST">
+              <!-- 	name	email	phone	reg_date -->
+              <div class="form-group">
+                <label for="">Name</label>
+                <input type="text" class="form-control" name="fullname" value="Latika" required />
+              </div>
+              <div class="form-group mt-3">
+                <label for="">Email</label>
+                <input type="email" class="form-control" name="email" value="itslatikajoshi@gmail.com" required />
+              </div>
+              <div class="form-group mt-3">
+                <label for="">Phone</label>
+                <input type="tel" class="form-control lj-phone" id="lj-phone" name="phone" value="8556909577" required />
+              </div>
+
+              <button type="submit" class="btn btn-primary mt-3 lj-submit">Submit</button>
+            </form>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-8">
+        <table class="table table-light border">
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Phone</th>
+            <th>Action</th>
+          </tr>
+
+          <?php
+          // function call so that data can be displayed
+          // read($conn); 
+          $sql = "SELECT * FROM `contacts` ORDER BY id DESC";
+          $result = $conn->query($sql);
+          $count = 1;
+          if ($result->num_rows > 0) {
+            // output data of each row
+            //   print_r($row);die();
+            while ($row = $result->fetch_assoc()) {
+
+              echo  "<tr>" . "<td>" . $count . "</td>" . "<td>" . $row["name"] . "</td>" . "<td>" . $row["email"] . "</td>" . "<td>" . $row["phone"] . "</td>" . "<td>" . "<a class='btn btn-secondary' href='./update.php?id=" . $row["id"] . "'> Update </a> " . "<a class='btn btn-danger cl-delete' href='./delete.php?id=" . $row["id"] . "'>Delete </a>" . "</td>" . "</tr>";
+
+              $count++;
+            }
+          } else {
+            echo "<tr><td colspan='5' class='text-center'>No result found</td></tr>";
+            // echo "0 results";
+          }
+          ?>
+        </table>
+      </div>
+    </div>
+  </div>
+
+  <script src="./script.js"></script>
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -1,101 +1,44 @@
 <?php
 include "./db-conn.php";
-include "./functions.php";
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>CRUD app</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Portal</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
 <body>
-  <div class="container mt-5">
-    <div class="row">
-      <div class="col-sm-4">
-        <!--
-            NOTES: By default form method is GET
-            Array
-            (
-            [fullname] => Latika
-            [email] => itslatika@gmail.com
-            [phone] => 876543345
-            )
-            -->
-        <div class="card">
-          <div class="card-header">
-            Please fill below details
-          </div>
-          <div class="card-body">
-            <?php
-            if (isset($_GET["error"])) : ?>
-              <div class="lj-errors">
-                <div class="alert alert-danger" role="alert">
-                 <?php echo $_GET["error"];?>
-                </div>
-              </div>
-            <?php
-            endif; ?>
-            <form action="create.php" class="lj-submit-form" method="POST">
-              <!-- 	name	email	phone	reg_date -->
-              <div class="form-group">
-                <label for="">Name</label>
-                <input type="text" class="form-control" name="fullname" value="Latika" required />
-              </div>
-              <div class="form-group mt-3">
-                <label for="">Email</label>
-                <input type="email" class="form-control" name="email" value="itslatikajoshi@gmail.com" required />
-              </div>
-              <div class="form-group mt-3">
-                <label for="">Phone</label>
-                <input type="tel" class="form-control lj-phone" id="lj-phone" name="phone" value="8556909577" required />
-              </div>
-
-              <button type="submit" class="btn btn-primary mt-3 lj-submit">Submit</button>
-            </form>
-          </div>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-sm-8">
+                <img src="https://miro.medium.com/v2/resize:fit:720/format:webp/1*2eBdh0vLZjUyCDF6x1EqvQ.png" alt="CRUD image">
+            </div>
+            <div class="col-sm-4">
+                <form>
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Email address</label>
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputPassword1">Password</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                    </div>
+                    <div class="form-group form-check">
+                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
         </div>
-      </div>
-      <div class="col-sm-8">
-        <table class="table table-light border">
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Phone</th>
-            <th>Action</th>
-          </tr>
 
-          <?php
-          // function call so that data can be displayed
-          // read($conn); 
-          $sql = "SELECT * FROM `contacts` ORDER BY id DESC";
-          $result = $conn->query($sql);
-          $count = 1;
-          if ($result->num_rows > 0) {
-            // output data of each row
-            //   print_r($row);die();
-            while ($row = $result->fetch_assoc()) {
-
-              echo  "<tr>" . "<td>" . $count . "</td>" . "<td>" . $row["name"] . "</td>" . "<td>" . $row["email"] . "</td>" . "<td>" . $row["phone"] . "</td>" . "<td>" . "<a class='btn btn-secondary' href='./update.php?id=" . $row["id"] . "'> Update </a> " . "<a class='btn btn-danger cl-delete' href='./delete.php?id=" . $row["id"] . "'>Delete </a>" . "</td>" . "</tr>";
-
-              $count++;
-            }
-          } else {
-            echo "<tr><td colspan='5' class='text-center'>No result found</td></tr>";
-            // echo "0 results";
-          }
-          ?>
-        </table>
-      </div>
     </div>
-  </div>
 
-  <script src="./script.js"></script>
 </body>
 
 </html>
